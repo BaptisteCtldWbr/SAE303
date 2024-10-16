5c85e763678628032385571ddd4d07ab35c46448
BaptisteCtldWbr <160064297+BaptisteCtldWbr@users.noreply.github.com>
2024-10-16 16:43:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function afficherFilms() {
-    const reponse = await fetch('https://github.com/BaptisteCtldWbr/SAE303/blob/main/geo-les_salles_de_cinemas_en_ile-de-france.json');
+    const reponse = await fetch('https://raw.githubusercontent.com/BaptisteCtldWbr/SAE303/refs/heads/main/geo-les_salles_de_cinemas_en_ile-de-france.json?token=GHSAT0AAAAAACY5XC2H6ZZCQTUJZG66MPVAZYP34SA');
     const films = await reponse.json();
     console.log(films[nom]);
   }
