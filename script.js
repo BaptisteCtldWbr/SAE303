2ab07df99ad559a7104cf69e60878e5f608dd78a
BaptisteCtldWbr <160064297+BaptisteCtldWbr@users.noreply.github.com>
2024-10-16 16:36:30 +0200

    ajout des pages

---
+++ script.js
@@ -0,0 +1,7 @@
+async function afficherFilms() {
+    const reponse = await fetch('geo-les_salles_de_cinemas_en_ile-de-france.json');
+    const films = await reponse.json();
+    console.log(films[nom]);
+  }
+
+afficherFilms();
