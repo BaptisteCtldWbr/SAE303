b2af81fc24142edcf8d2160318ad564783b75619
benam-amin <benamaouche.amin@gmail.com>
2024-10-21 11:11:38 +0200

    modification index et donnees.js
    suppression de l'attribut defer + passage des balises script a la fin du body + changement de la fonction asynchrone qui retourne désormais la reponse du fetch

--- donnees.js
+++ donnees.js
@@ -1,11 +1,14 @@
-async function afficherFilms() {
-    const reponse = await fetch('geo-les_salles_de_cinemas_en_ile-de-france.json');
-    const films = await reponse.json();
-    console.log(films);
+async function recupererDonnes(url) {
+    const reponse = await fetch(url);
+    const cinemas = await reponse.json();
+    return cinemas
   }
 
-afficherFilms(); 
-let map = L.map('map').setView([51.505, -0.09], 13);
+const donnesCinemas = recupererDonnes('geo-les_salles_de_cinemas_en_ile-de-france.json'); 
+console.log(donnesCinemas); 
+
+let map = L.map('map').setView([48.6788809 , 1.8419643], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
