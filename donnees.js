6d6637e02b07dc4f37bc909aaa06d9393d024fa0
benam-amin <benamaouche.amin@gmail.com>
2024-10-21 15:46:56 +0200

    Update donnees.js
    ajout d'une conversion des données en geoJSON

--- donnees.js
+++ donnees.js
@@ -1,15 +1,93 @@
-async function recupererDonnes(url) {
-    const reponse = await fetch(url);
-    const cinemas = await reponse.json();
-    return cinemas
+  function initMap() {
+    let map = L.map('map').setView([48.8566, 2.3522], 12); // Paris
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+    return map;
+  }
+function recupererDonnes(url) {
+    return fetch(url)
+      .then(response => response.json()) // Convertir en JSON
+      .catch(error => {
+        console.error('Erreur lors du chargement du fichier JSON:', error);
+      });
   }
 
-const donnesCinemas = recupererDonnes('geo-les_salles_de_cinemas_en_ile-de-france.json'); 
-console.log(donnesCinemas); 
+  // Fonction pour convertir les données JSON en GeoJSON
+  function convertToGeoJSON(data) {
+    return data.map(function(cinema) {
+      let coords = cinema.geo.split(',').map(Number); // Séparer les coordonnées lat, long
 
-let map = L.map('map').setView([48.6788809 , 1.8419643], 13);
+      return {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [coords[1], coords[0]] // [longitude, latitude]
+        },
+        "properties": {
+          "ndeg_auto": cinema.ndeg_auto,
+          "dep": cinema.dep,
+          "nom": cinema.nom,
+          "region_administrative": cinema.region_administrative,
+          "adresse": cinema.adresse,
+          "code_insee": cinema.code_insee,
+          "commune": cinema.commune,
+          "situation_geographique": cinema.situation_geographique,
+          "ecrans": cinema.ecrans,
+          "fauteuils": cinema.fauteuils,
+          "semaines_d_activite": cinema.semaines_d_activite,
+          "seances": cinema.seances,
+          "entrees": cinema.entrees,
+          "entrees_2015": cinema.entrees_2015,
+          "evolution_entrees": cinema.evolution_entrees,
+          "tranche_d_entrees": cinema.tranche_d_entrees,
+          "proprietaire": cinema.proprietaire,
+          "programmateur": cinema.programmateur,
+          "ae": cinema.ae,
+          "categorie_art_et_essai": cinema.categorie_art_et_essai,
+          "label_art_et_essai": cinema.label_art_et_essai,
+          "genre": cinema.genre,
+          "multiplexe": cinema.multiplexe,
+          "nombre_de_films_programmes": cinema.nombre_de_films_programmes,
+          "nombre_de_films_inedits": cinema.nombre_de_films_inedits,
+          "nombre_de_films_en_semaine_1": cinema.nombre_de_films_en_semaine_1,
+          "pdm_en_entrees_des_films_francais": cinema.pdm_en_entrees_des_films_francais,
+          "pdm_en_entrees_des_films_americains": cinema.pdm_en_entrees_des_films_americains,
+          "pdm_en_entrees_des_films_europeens": cinema.pdm_en_entrees_des_films_europeens,
+          "pdm_en_entrees_des_autres_films": cinema.pdm_en_entrees_des_autres_films,
+          "films_art_et_essai": cinema.films_art_et_essai,
+          "part_des_seances_de_films_art_et_essai": cinema.part_des_seances_de_films_art_et_essai,
+          "pdm_en_entrees_des_films_art_et_essai": cinema.pdm_en_entrees_des_films_art_et_essai
+        }
+      };
+    });
+  }
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+  // Fonction pour ajouter les données GeoJSON à la carte
+  function addDataToMap(map, geoJSONData) {
+    L.geoJSON(geoJSONData, {
+      onEachFeature: function (feature, layer) {
+        let popupContent = "<b>" + feature.properties.nom + "</b><br>" +
+                           "Adresse : " + feature.properties.adresse + "<br>" +
+                           "Commune : " + feature.properties.commune + "<br>" +
+                           "Ecrans : " + feature.properties.ecrans + "<br>" +
+                           "Fauteuils : " + feature.properties.fauteuils + "<br>" +
+                           "Propriétaire : " + feature.properties.proprietaire + "<br>" +
+                           "Entrées : " + feature.properties.entrees + "<br>" +
+                           "Evolution des entrées : " + feature.properties.evolution_entrees + "%";
+        layer.bindPopup(popupContent);
+      }
+    }).addTo(map);
+  }
+
+
+  // Exécution du script en style procédural
+  let map = initMap(); // Étape 1 : Initialiser la carte
+  recupererDonnes('geo-les_salles_de_cinemas_en_ile-de-france.json') // Étape 2 : Charger les données
+    .then(data => {
+      let geoJSONData = {
+        "type": "FeatureCollection",
+        "features": convertToGeoJSON(data) // Étape 3 : Conversion en GeoJSON
+      };
+      addDataToMap(map, geoJSONData); // Étape 4 : Ajouter les données sur la carte
+    });
