cc45315f4a421556ca6f0a1e2c50a3721c0870b1
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-11-14 15:37:50 +0100

    Annule changement fond de carte

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 // Initialiser la carte
 function initMap() {
   let map = L.map('map').setView([48.8566, 2.3522], 12); // Paris
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors',
     minZoom: 0,
     maxZoom: 20
   }).addTo(map);
